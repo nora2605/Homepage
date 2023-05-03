ecf2984d7d6cfe6f106b5977968786cd5bf1a6bd
Nora J.F <nora.ja2605@gmail.com>
2023-05-03 14:35:34 +0200

    added vercel compatibility adapter and meta tags idk

--- src/root.tsx
+++ src/root.tsx
@@ -49,6 +49,11 @@ export default function Root() {
         <Title>SolidStart - With TailwindCSS</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Meta property="og:title" content={`Lümi Home - ${location.pathname === "/" ? "Home" : location.pathname.substring(1).toUpperCase()}`} />
+        <Meta property="og:description" content={`literally me when ${location.pathname === "/" ? "Home" : location.pathname.substring(1).toUpperCase()}`} />
+        <Meta property="og:image" content={`/favicon.ico`} />
+        <Meta property="og:url" content={"https://luemir.ml" + location.pathname} />
+        <Meta property="og:site_name" content="Lümi Home" />
       </Head>
       <Body>
         <Suspense>
