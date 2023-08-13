726f5e93833053f49b42d18186c78b67cd6a65fd
Nora J.F <nora.ja2605@gmail.com>
2023-08-13 05:28:08 +0200

    minor api discrepancy

--- src/components/TrackList.tsx
+++ src/components/TrackList.tsx
@@ -1,7 +1,6 @@
 import { For, createEffect, createMemo, createSignal, onCleanup, onMount } from 'solid-js';
 import { A, useNavigate } from 'solid-start';
 import TrackDetail from './TrackDetail';
-import { Dynamic } from 'solid-js/web';
 
 export default function TrackList({ tracks }: { tracks: Track[] }) {
     const navigate = useNavigate();
@@ -22,7 +21,7 @@ export default function TrackList({ tracks }: { tracks: Track[] }) {
     const sortedTracks = createMemo(() =>
     tracks
       .filter((track) =>
-        track.title.toLowerCase().includes(searchQuery().toLowerCase())
+        track.name.toLowerCase().includes(searchQuery().toLowerCase())
       )
       .sort((a, b) => {
         const compareValueA = a[sortKey() as keyof Track]?.toLocaleString() ?? '';
@@ -91,7 +90,7 @@ export default function TrackList({ tracks }: { tracks: Track[] }) {
 
 export interface Track {
     id: string,
-    title: string,
+    name: string,
     album?: string,
     genre?: string,
     duration?: number,
